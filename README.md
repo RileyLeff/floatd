c97c8c944fe7727dbbbd266a13a26df22f647898
RileyLeff <rileyleff@gmail.com>
2024-03-24 13:35:20 -0400

    improved readme

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Tiny crate that provides a "FloatD" trait, i.e. num_traits::Float + Debug + Disp
 This is likely to be subsumed into a successor crate once my project is more mature.
 For now, this is the most convenient place to keep the trait.
 
+According to the rust api guidelines chapter on [futureproofing](https://rust-lang.github.io/api-guidelines/future-proofing.html)
+specifying Display and Debug as trait bounds is bad practice.
+
+I think my downstream use case for this crate (guaranteeing a struct's underlying data can be formatted into an associated error type)
+falls under exception 1 listed in the guidelines. Your mileage may vary, though.
+
 ## Compatibility
 
 This is a no-std crate by default, with std support behind a feature flag.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 //! Tiny crate that provides a "FloatD" trait, i.e. num_traits::Float + Debug + Display.
 //! This is likely to be subsumed into a successor crate once my project is more mature.
 //! For now, this is the most convenient place to keep the trait.
+//!
+//! According to the rust api guidelines chapter on [futureproofing](https://rust-lang.github.io/api-guidelines/future-proofing.html)
+//! specifying Display and Debug as trait bounds is bad practice.
+//!
+//! I think my downstream use case for this crate (guaranteeing a struct's underlying data can be formatted into an associated error type)
+//! falls under exception 1 listed in the guidelines. Your mileage may vary, though.
 
 // configure no_std if both std_math and std_errors features are inactive
 #![cfg_attr(not(feature = "std"), no_std)]
